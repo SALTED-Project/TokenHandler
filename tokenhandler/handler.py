53a1d8da355360eb66693d543fe4793ee7148917
Víctor González <vgonzalez@tlmat.unican.es>
2023-06-02 11:35:38 +0200

    Restructured methods and minimized arguments

--- tokenhandler/handler.py
+++ tokenhandler/handler.py
@@ -5,31 +5,27 @@ from datetime import datetime
 class TokenHandler():
     """Handles the interaction with the authentication provider, that is necessary to obtain an up to date token."""
 
-    def __init__(self, token_endpoint, auth_client_id, auth_client_secret, scope= 'salted', grant_type= 'client_credentials', token_expiry_time = 0):
+    def __init__(self, token_endpoint: str, auth_client_id: str, auth_client_secret: str):
         """Initializes TokenHandler.
         
         token_endpoint (str): URL specifying the authentication endpoint.
         auth_client_id (str): Keycloak Client ID.
         auth_client_secret (str): Keycloak Client Secret.
-        scope (str): Keycloak Scope.
-        grant_type (str): Keycloak Grant Type used.
-        token_expiry_time (int): Expiry Time of Token.
         """
         self.__token_endpoint = token_endpoint
         self.__data = {
-            'grant_type': grant_type,
+            'grant_type': 'client_credentials',
             'client_id': auth_client_id,
             'client_secret': auth_client_secret,
-            'scope': scope
+            'scope': 'salted'
         }
-        self.__token_expiry_time = token_expiry_time
+        self.__token_expiry_time = 0
         self.__token = ''        
         self.__headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     
-    def update_token(self) -> None:
+    def __update_token(self) -> None:
         """
         Update token if the current one has expired.
-        Returns valid token.
         """
         if (self.__token_expiry_time > datetime.timestamp(datetime.now())): return
         res = requests.post(self.__token_endpoint, headers=self.__headers, data=self.__data)
@@ -38,7 +34,10 @@ class TokenHandler():
         self.__token = res_dict["access_token"]
         self.__token_expiry_time = datetime.timestamp(datetime.now()) + res_dict["expires_in"] - 10
 
+    def get_token(self) -> str:
+        """
+        Returns an up-to-date valid token.
+        """
+        self.__update_token()
         return self.__token
-    
-    
     
